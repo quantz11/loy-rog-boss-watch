9372325f5fd267bad0f1a84a9f1d2c619e87f069
Quantz <gdbayucan@live.com.ph>
2026-01-08 13:34:30 +0800

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
+
 import type {NextConfig} from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+// Replace '<repo-name>' with the name of your GitHub repository.
+const repoName = 'loy-rog-boss-watch'; 
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  // Set basePath only for production builds on GitHub Pages.
+  basePath: isProd ? `/${repoName}` : '',
+  // Set assetPrefix to ensure assets (CSS, JS, images) are loaded correctly.
+  assetPrefix: isProd ? `/${repoName}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
