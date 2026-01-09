2481255735e4a875f29eb4ef9af24ecece38186c
Quantz <gdbayucan@live.com.ph>
2026-01-09 12:10:36 +0800

    Update keys

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6410704779-3f31c",
-  "appId": "1:631939362850:web:d2814604a6a8af43945a7b",
-  "apiKey": "AIzaSyCGl3HpWVINdHhDdD2m3IBNlcZSQm-ukB0",
-  "authDomain": "studio-6410704779-3f31c.firebaseapp.com",
+  "projectId": "projectId",
+  "appId": "appId",
+  "apiKey": "apiKey",
+  "authDomain": "authDomain",
   "measurementId": "",
-  "messagingSenderId": "631939362850"
+  "messagingSenderId": "messagingSenderId"
 };
